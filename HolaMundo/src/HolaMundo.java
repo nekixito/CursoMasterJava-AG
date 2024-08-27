2d17a1ab8addda65e16df11dccd2580cace3217c
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2024-08-27 12:21:31 -0600

    Ejemplos de variables

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,5 +1,34 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo con Java");
+
+        String saludar = "Hola Mundo desde java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        
+        boolean valor = true;
+        int numero2 = 10;
+        
+        if(valor){
+            System.out.println("valor = " + valor);
+            System.out.println("numero2 = " + numero2);
+        }
+
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
+        System.out.println("numero3 = " + numero3);
+        
+        String nombre;
+        
+        nombre = "Miguel";
+        
+        if (numero > 9){
+            nombre = "Antony";
+        }
+
+        System.out.println("nombre = " + nombre);
     }
 }
